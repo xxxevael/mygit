343c12019f988a5a5539c93c9791e89a2509b9d1
‘xxxevael <495319077@qq.com>
2022-03-21 15:48:43 +0800

    vue-router 以及迷你router的封装

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter,createWebHashHistory} from 'vue-router'
+// import { createRouter,createWebHashHistory} from './grouter/index'
 //当引入页面/pages/About.vue 页面的名称大小写与实际页面的名称大小写不同时 vite热更新会失效
 import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
